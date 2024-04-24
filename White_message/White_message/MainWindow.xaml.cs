f7a0a331de690fa82de0dadb8a7c10b1181de199
Maksim Vorontsov <hhiummm@mail.ru>
2024-04-24 10:21:16 +0300

    pre MVP

---
+++ White_message/White_message/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace White_message
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            string serverIP = "127.0.0.1";
+
+            // Укажите порт сервера
+            int port = 8000;
+
+            // Создайте сокет TCP
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Подключитесь к серверу
+            clientSocket.Connect(IPAddress.Parse(serverIP), port);
+
+            // Отправьте сообщение серверу
+            string message = Name.Text;
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            clientSocket.Send(buffer);
+            listen(clientSocket);
+
+            // Закройте сокет
+            //clientSocket.Close();
+        }
+        async void listen(Socket clientSocket)
+        {
+            // Получите ответ от сервера
+            byte[] buffer = new byte[1024];
+            int bytesReceived = clientSocket.Receive(buffer);
+            string replyMessage = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+            // Выведите ответ сервера
+            Chat.Text += replyMessage+"\n";
+            await Task.Delay(500);
+            listen(clientSocket);
+        }
+    }
+}
---
+++ White_server/White_server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace White_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.listen();          
+        }
+        List<Socket> Users = new List<Socket>();
+        public void listen()
+        {
+            // Укажите порт для прослушивания
+            int port = 8000;
+            // Создайте сокет TCP
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // Привяжите сокет к порту
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            while (true)
+            {
+                // Прослушивайте входящие соединения
+                serverSocket.Listen(10);
+
+                Console.WriteLine($"Сервер запущен на порту {port}. Ожидаем подключения...");
+
+                // Примите подключение от клиента
+                Socket clientSocket = serverSocket.Accept();
+
+                // Получите данные от клиента
+                byte[] buffer = new byte[1024];
+                int bytesReceived = clientSocket.Receive(buffer);
+
+                // Преобразуйте данные в строку
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+                Users.Add(clientSocket);
+                // Выведите сообщение от клиента
+                for (int i = 0; i < Users.Count; i++)
+                {
+                    // Отправьте сообщение серверу клиенту
+                    string replyMessage = ($"new user {message} joined");
+                    byte[] replyBuffer = Encoding.ASCII.GetBytes(replyMessage);
+                    Users[i].Send(replyBuffer);
+                }
+                Console.WriteLine($"new user {message}");
+                // Закройте сокеты
+                //clientSocket.Close();
+                //serverSocket.Close();
+            }
+
+        }
+    }
+}
